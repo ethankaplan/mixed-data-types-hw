e2634901f78992afe82fde72fb82ca4be7e970be
Ethan <N/A>
2019-03-25 16:09:13 -0700

    data structures

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 {
-    // Light switch: boolean
-    // Email: string
-    // spaceship: object
-    // student list: array
-    // student list with location: array of objects
-    // student list with more info: array of objects, which contain an array
+  // Light switch: boolean
+  // Email: string
+  // spaceship: object
+  // student list: array
+  // student list with location: array of objects, name and location
+  // student list with more info: array of objects, name, location, and an array of shows
 }
 
+{
+  let rainbow = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet"
+  ];
+  rainbow[4];
+  const Ethan = {
+    favFood: "pizza",
+    hobby: "photography",
+    town: "LA",
+    datatype: "bool"
+  };
+  Ethan.hobby;
+}
+
+{
+
+}
