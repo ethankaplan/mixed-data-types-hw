6d5f70085ee35a8cba824436de24c39ff5cffcf5
Ethan <N/A>
2019-03-25 16:00:32 -0700

    data types

---
+++ js/app.js
@@ -0,0 +1,9 @@
+{
+    // Light switch: boolean
+    // Email: string
+    // spaceship: object
+    // student list: array
+    // student list with location: array of objects
+    // student list with more info: array of objects, which contain an array
+}
+
