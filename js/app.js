e8f097191433059e574e42e01cea5451e3180958
Ethan <N/A>
2019-03-25 16:32:27 -0700

    object-ception

--- js/app.js
+++ js/app.js
@@ -66,3 +66,23 @@
       console.log(crazyObject.larry.characters);
 }
 
+{
+    const inception = {
+        reality: {
+            dreamLayer1: {
+                dreamLayer2: {
+                    dreamLayer3: {
+                        dreamLayer4: {
+                            dreamLayer5: {
+                                dreamLayer6: {
+                                    limbo: "Joseph Gordon Levitt"
+                               }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+     }
+    inception.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo=null;
+}
